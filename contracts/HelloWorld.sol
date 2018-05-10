917eeb1f834f6217967b05e754f703f9437037a0
Gochan57 <gochan@mail.ru>
2018-05-10 22:14:27 +0300

    деплой смарт контракта HelloWorld

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,7 +1,17 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.18;
 
 contract HelloWorld {
-    function HelloWorld(){
-
+    constructor(string name) public {
+        m_name = name;
     }
+
+    function modify_name(string new_name) public {
+        m_name = new_name;
+    }
+
+    function hello() public view returns (string) {
+        return m_name;
+    }
+
+    string m_name;
 }
--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,15 +1,8 @@
 'use strict';
 
-const _owners = [
-    '0xaacf78f8e1fbdcf7d941e80ff8b817be1f054af4',
-    '0xcb0ac3ce1e27c0b26dcf4ccd4a6891ed93b487fe',
-    '0x83d262fbf830065acd397c2f8548ce859da6e7d1'
-];
-
-
-const SmartzToken = artifacts.require('SmartzToken.sol');
+const HelloWorld = artifacts.require('HelloWorld.sol');
 
 
 module.exports = function(deployer, network) {
-    deployer.deploy(SmartzToken, _owners, 2);
+    deployer.deploy(HelloWorld, 'Gosha');
 };
